0073c23857f72e30addf9a447309d04eb6071b97
Satyam Kumar <silentvoice4143@gmail.com>
2023-12-30 19:21:03 +0530

    latest

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const route = require("./routes/route.js");
 const mongoose = require("mongoose");
-const multer = require("multer")
+const multer = require("multer");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.json());
 
+// Use body-parser middleware to parse URL-encoded and JSON bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(multer().any());
 
-mongoose.connect("mongodb+srv://poushali26:0U8on2StHP5FNKo2@cluster0.jwwwcc8.mongodb.net/baoiam")
+mongoose
+  .connect(
+    "mongodb+srv://poushali26:0U8on2StHP5FNKo2@cluster0.jwwwcc8.mongodb.net/baoiam"
+  )
 
   .then(() => console.log("MongoDb is connected"))
   .catch((err) => console.log(err));
 
+app.use(express.static("public"));
 
 app.use("/", route);
-
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Express app running on port " + (process.env.PORT || 3000));
--- routes/route.js
+++ routes/route.js
@@ -1,19 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require("../controller/userController")
+const userController = require("../controller/userController");
 
-const middleWare = require("../middleware/auth")
+const middleWare = require("../middleware/auth");
 
 //===============================userapi============================================//
 
-router.post("/register",userController.createUser)
-router.post("/login",userController.loginUser)
-router.get("/user/:userId/profile",middleWare.authentication,middleWare.authorization,userController.getuserprofile)
+router.post("/register", userController.createUser);
+router.post("/login", userController.loginUser);
+router.get(
+  "/user/:userId/profile",
+  middleWare.authentication,
+  middleWare.authorization,
+  userController.getuserprofile
+);
 
+router.post("/getOTP", userController.getOTP);
+router.post("/booksession", userController.booksession);
+router.get("/", (req, res) => {
+  res.sendFile("index.js");
+});
 
-router.all("/*",(req,res)=>{
-  res.status(400).send({status:false,message:"Endpoint is not correct"})})
-
-
+router.all("/*", (req, res) => {
+  res.status(400).send({ status: false, message: "Endpoint is not correct" });
+});
 
 module.exports = router;
